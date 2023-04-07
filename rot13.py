5f26e97a926d7b1ca9e8fdb0f331957fa6376ef7
N3koKawaii <99773781+N3koKawaii@users.noreply.github.com>
2023-04-07 09:58:26 -0400

    Add files via upload

---
+++ rot13.py
@@ -0,0 +1,16 @@
+def rot13(text):
+    ciphered_text = ""
+    for c in text:
+        ascii_c = ord(c)
+        if c.isalpha():
+            if ord("a") <= ord(c) <= ord("m") or ord("A") <= ord(c) <= ord("M"):
+                ascii_c = ord(c) + 13
+            else:
+                ascii_c = ord(c) - 13
+        ciphered_text += chr(ascii_c)
+    return ciphered_text
+
+
+print("Now you are using ROT13 substitution cipher.")
+text = input("Your text: ")
+print(rot13(text))
